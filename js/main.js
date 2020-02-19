76340af7083efbd848383d13045a16d796c06e5c
federico filella <federico.filella@gmail.com>
2020-02-19 22:24:06 +0100

    first

---
+++ js/main.js
@@ -0,0 +1,33 @@
+// ● Chiedi all’utente la sua email,
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato
+
+var emailToTheParty = ["MarySwell@gmail.com", "FujiwaraTakeshi@gmail.com", "MarcoLombardi@gmail.com", "AdelmioSabbatini@gmail.com", "FoilirLongbeard@gmail.com"]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ● Gioco dei dadi, chi fa di più vince
