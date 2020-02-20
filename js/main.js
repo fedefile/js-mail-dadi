ea39b9bb98c1c9e454a1692330f60c01d81398e0
federico filella <federico.filella@gmail.com>
2020-02-20 13:19:18 +0100

    js update

--- js/main.js
+++ js/main.js
@@ -1,27 +1,39 @@
 // ● Chiedi all’utente la sua email,
-// controlla che sia nella lista di chi può accedere,
-// stampa un messaggio appropriato
 
-var emailToTheParty = ["MarySwell@gmail.com", "FujiwaraTakeshi@gmail.com", "MarcoLombardi@gmail.com", "AdelmioSabbatini@gmail.com", "FoilirLongbeard@gmail.com"]
-var guest = prompt ("Enter Name")
-var realGuest = true || false;
+// 1- controlla che sia nella lista di chi può accedere,
+// 2- stampa un messaggio appropriato
+
+var emailToTheParty = ["MarySwell@gmail.com", "FujiwaraTakeshi@gmail.com", "MarcoLombardi@gmail.com", "AdelmioSabatini@gmail.com"];
+var guest = prompt ("Enter Name");
+
+console.log(emailToTheParty);
+console.log(guest);
 
 for (var i = 0; i < emailToTheParty.length; i++) {
-    console.log(emailToTheParty[i])
-    if (enterName == emailToTheParty[i]) {
-        guest = true
-
+    if (guest == emailToTheParty[i]) {
+        console.log("Welcome here!");
     }
-    if else (enterName != emailToTheParty[i]) {
-        guest = false {
+    if else {
+        console.log("You're not a guest");
     }
+}
 
 
+// ● Gioco dei dadi, chi fa di più vince.
+// Questo gioco di dadi vede come giocatori utente e macchina.
 
 
+var primo numero = Math.floor(Math.random() * 6) + 1;
+var secondo numero = Math.floor(Math.random() * 6) + 1;
+console.log(primo numero);
+console.log(secondo numero);
 
-
-
-
-// ● Gioco dei dadi, chi fa di più vince
-// da implementare
+if (primo numero > secondo numero) {
+    console.log("Vince player1");
+}
+else if (primo numero < secondo numero) {
+    console.log("Vince player2");
+}
+else {
+console.log("Parità!!!");    
+}
